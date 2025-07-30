8e2b5546ffa4324dc0ab482b4866ce5dae13541d
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2025-07-30 17:15:58 +0900

    modified diff

--- diff/app/src/main/java/org/example/App.java
+++ diff/app/src/main/java/org/example/App.java
@@ -6,79 +6,77 @@ import java.util.Objects;
 
 public class App {
     public static void main(String[] args) {
-        List<String> orig = List.of("a", "b", "c", "d", "e", "f");
+        List<String> org = List.of("a", "b", "c", "d", "e", "f");
         List<String> rev = List.of("d", "a", "c", "f", "e", "a");
-        List<Change> changes = buildRevision(buildPath(orig, rev), orig, rev);
+        List<Change> changes = buildRevision(buildPath(org, rev));
         changes.forEach(System.out::println);
     }
 
     record PathNode(int i, int j, boolean snake, boolean bootstrap, PathNode prev) {
-        PathNode(int i, int j, boolean snake, boolean bootstrap, PathNode prev) {
-            this.i = i;
-            this.j = j;
-            this.snake = snake;
-            this.bootstrap = bootstrap;
-            this.prev = snake ? prev : prev == null ? null : prev.previousSnake();
+        PathNode() {
+            this(0, -1, true, true, null);
         }
-        PathNode previousSnake() {
-            if (bootstrap) {
-                return null;
-            }
-            if (!snake && prev != null) {
-                return prev.previousSnake();
-            }
-            return this;
+        PathNode(int i, int j, boolean snake, PathNode prev) {
+            this(i, j, snake, false,
+                snake ? prev : prev == null ? null : prev.prevSnake());
+        }
+        PathNode prevSnake() {
+            return bootstrap ? null
+                : (!snake && prev != null)
+                    ? prev.prevSnake()
+                    : this;
         }
     }
 
-    record Change(char deltaType, int startOriginal, int endOriginal, int startRevised, int endRevised) { }
+    record Change(Type type, int startOriginal, int endOriginal, int startRevised, int endRevised) { }
+    enum Type { CHANGE, DELETE, INSERT }
 
     private static PathNode buildPath(final List<String> orig, final List<String> rev) {
 
-        final int N = orig.size();
-        final int M = rev.size();
+        final int n = orig.size();
+        final int m = rev.size();
 
-        final int MAX = N + M + 1;
-        final int size = 1 + 2 * MAX;
+        final int max = n + m + 1;
+        final int size = 1 + 2 * max;
         final int middle = size / 2;
         final PathNode[] diagonal = new PathNode[size];
 
-        diagonal[middle + 1] = new PathNode(0, -1, true, true, null);
-        for (int d = 0; d < MAX; d++) {
+        diagonal[middle + 1] = new PathNode();
+        for (int d = 0; d < max; d++) {
             for (int k = -d; k <= d; k += 2) {
-                final int kmiddle = middle + k;
-                final int kplus = kmiddle + 1;
-                final int kminus = kmiddle - 1;
+                final int kMiddle = middle + k;
+                final int kPlus = kMiddle + 1;
+                final int kMinus = kMiddle - 1;
                 PathNode prev;
                 int i;
 
-                if ((k == -d) || (k != d && diagonal[kminus].i < diagonal[kplus].i)) {
-                    i = diagonal[kplus].i;
-                    prev = diagonal[kplus];
+                if ((k == -d) || (k != d && diagonal[kMinus].i < diagonal[kPlus].i)) {
+                    i = diagonal[kPlus].i;
+                    prev = diagonal[kPlus];
                 } else {
-                    i = diagonal[kminus].i + 1;
-                    prev = diagonal[kminus];
+                    i = diagonal[kMinus].i + 1;
+                    prev = diagonal[kMinus];
                 }
 
-                diagonal[kminus] = null; // no longer used
+                diagonal[kMinus] = null; // no longer used
 
                 int j = i - k;
 
-                PathNode node = new PathNode(i, j, false, false, prev);
+                PathNode node = new PathNode(i, j, false, prev);
 
-                while (i < N && j < M && Objects.equals(orig.get(i), rev.get(j))) {
+                while (i < n && j < m && Objects.equals(orig.get(i), rev.get(j))) {
                     i++;
                     j++;
                 }
 
                 if (i != node.i) {
-                    node = new PathNode(i, j, true, false, node);
+                    node = new PathNode(i, j, true, node);
                 }
 
-                diagonal[kmiddle] = node;
+                diagonal[kMiddle] = node;
 
-                if (i >= N && j >= M) {
-                    return diagonal[kmiddle];
+                if (i >= n && j >= m) {
+                    return diagonal[kMiddle];
                 }
             }
             diagonal[middle + d - 1] = null;
@@ -86,33 +84,28 @@ public class App {
         throw new IllegalStateException("could not find a diff path");
     }
 
-    private static List<Change> buildRevision(PathNode actualPath, List<String> orig, List<String> rev) {
+    private static List<Change> buildRevision(PathNode path) {
 
-        PathNode path = actualPath;
         List<Change> changes = new ArrayList<>();
         if (path.snake) {
             path = path.prev;
         }
         while (path != null && path.prev != null && path.prev.j >= 0) {
-            if (path.snake()) {
-                throw new IllegalStateException("bad diffpath: found snake when looking for diff");
+            if (path.snake) {
+                throw new IllegalStateException("illegal path");
             }
             int i = path.i;
             int j = path.j;
 
             path = path.prev;
-            int ianchor = path.i;
-            int janchor = path.j;
+            Type type = (path.i == i && path.j != j)
+                ? Type.INSERT
+                : (path.i != i && path.j == j)
+                    ? Type.DELETE
+                    : Type.CHANGE;
+            changes.addFirst(new Change(type, path.i, i, path.j, j));
 
-            if (ianchor == i && janchor != j) {
-                changes.add(new Change('I', ianchor, i, janchor, j));
-            } else if (ianchor != i && janchor == j) {
-                changes.add(new Change('D', ianchor, i, janchor, j));
-            } else {
-                changes.add(new Change('C', ianchor, i, janchor, j));
-            }
-
-            if (path.snake()) {
+            if (path.snake) {
                 path = path.prev;
             }
         }
